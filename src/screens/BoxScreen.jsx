50fe4c071ac229b5c13775f6294c84bf33ab9486
Mostafa Saeidi <msaiedimoien@yahoo.com>
2022-09-01 20:18:47 +0430

    In BoxScreen exercise: 1. Box Object Model 2. Flex Box: --- alignItems --- flexDirection

--- src/screens/BoxScreen.jsx
+++ src/screens/BoxScreen.jsx
@@ -1,14 +1,95 @@
 import React from "react";
-import { Text,View,StyleSheet } from "react-native";
+import { Text,View,StyleSheet, ScrollView } from "react-native";
 
 const BoxScreen = () => {
-    return(
-        <View>
+    return (
+        <ScrollView>
+            <Text style={styles.titleStyle}>Box Object Model:</Text>
+            <View style={styles.viewStyle}>
+                <Text style={styles.bomText}>
+                    margin\border\padding\CONTENT
+                </Text>
+            </View>
 
-        </View>
-    )
-}
+            <Text style={styles.titleStyle}>Flex Box:</Text>
+            <View style={styles.viewStyle}>
+                <Text style={styles.Child1Text}>Child #1</Text>
+                <Text style={styles.Child2Text}>in parentStyle --> alignItems: 'stretch' (default)</Text>
+                <Text style={styles.Child3Text}>Child #3</Text>
+            </View>
 
-const styles = StyleSheet.create({});
+            <View style={[{alignItems: 'flex-start'}, styles.viewStyle]}>
+                <Text style={styles.Child1Text}>Child #1</Text>
+                <Text style={styles.Child2Text}>in parentStyle --> alignItems: 'flex-start'</Text>
+                <Text style={styles.Child3Text}>Child #3</Text>
+            </View>
+
+            <View style={[{alignItems: 'center'}, styles.viewStyle]}>
+                <Text style={styles.Child1Text}>Child #1</Text>
+                <Text style={styles.Child2Text}>in parentStyle --> alignItems: 'center'</Text>
+                <Text style={styles.Child3Text}>Child #3</Text>
+            </View>
+
+            <View style={[{alignItems: 'flex-end'}, styles.viewStyle]}>
+                <Text style={styles.Child1Text}>Child #1</Text>
+                <Text style={styles.Child2Text}>in parentStyle --> alignItems: 'flex-end'</Text>
+                <Text style={styles.Child3Text}>Child #3</Text>
+            </View>
+
+            <View style={[{flexDirection: 'column'}, styles.viewStyle]}>
+                <Text style={styles.Child1Text}>Child #1</Text>
+                <Text style={styles.Child2Text}>in parentStyle --> flexDirection: 'column'</Text>
+                <Text style={styles.Child3Text}>Child #3</Text>
+            </View>
+
+            <View style={[{flexDirection: 'row', height: 100}, styles.viewStyle]}>
+                <Text style={styles.Child1Text}>Child #1</Text>
+                <Text style={styles.Child2Text}>in parentStyle --> flexDirection: 'row'</Text>
+                <Text style={styles.Child3Text}>Child #3</Text>
+            </View>
+        </ScrollView>
+    );
+};
+
+const styles = StyleSheet.create({
+    titleStyle: {
+        paddingLeft: 10,
+        paddingTop: 10,
+    },
+
+    viewStyle: {
+        margin: 10,
+        padding: 5,
+        marginBottom: 30,
+        // borderWidth: 1,
+        borderColor: 'brown',
+    },
+
+    bomText: {
+        margin: 25,
+        borderWidth: 15,
+        borderColor: 'greenyellow',
+        paddingLeft: 50,
+        paddingTop: 30,
+    },
+
+    Child1Text: {
+        borderWidth: 1,
+        borderColor: 'red',
+        backgroundColor: 'yellow'
+    },
+
+    Child2Text: {
+        borderWidth: 1,
+        borderColor: 'red',
+        backgroundColor: 'aqua'
+    },
+
+    Child3Text: {
+        borderWidth: 1,
+        borderColor: 'red',
+        backgroundColor: 'greenyellow'
+    },
+});
 
 export default BoxScreen;
